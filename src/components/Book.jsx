e183f657d94f074894a2d3fb002c90249fe0fa08
AmpatzidisSavvas <savvas.ampa@gmail.com>
2025-06-21 10:53:22 +0300

    first commit

---
+++ src/components/Book.jsx
@@ -0,0 +1,51 @@
+
+import HTMLFlipBook from "react-pageflip";
+
+function Book() {
+  const imageList = Array.from({ length: 36 }, (_, i) => {
+    const number = String(i + 1).padStart(3, '0'); 
+    return `Balkanoia1_${number}.jpg`;
+  });
+  
+  return (
+    
+      <HTMLFlipBook 
+        width={420}          
+        height={595}         
+        maxShadowOpacity={0.5}
+        size="stretch"       
+        minWidth={298}        
+        minHeight={420}   
+        maxWidth={420}        
+        maxHeight={595}    
+        drawShadow={true}
+        showCover={true}
+        
+      >
+        <div className="page" style={{ background: 'transparent' }}>
+          <div className="page-content cover">
+            <img 
+              src='/img/Balkanoia1_000.jpg' 
+              alt="Pokémon Logo" 
+              className="cover-img"
+            />
+          </div>
+        </div>
+
+        {imageList.map((filename, index) => (
+          <div className="page" key={index}>
+            <div className="page-content" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+              <img 
+                src={`/img/${filename}`} 
+                alt={filename} 
+                style={{ maxWidth: '100%', maxHeight: '100%' }}
+              />
+            </div>
+          </div>
+        ))}
+      </HTMLFlipBook>
+  
+  );
+}
+
+export default Book
